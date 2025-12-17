56a1fe7b56199af64bea59fb9501373cf132e498
audh25-lab <audh25@gmail.com>
2025-12-18 00:40:15 +0500

    Update PoliceSystem.ts

--- src/systems/PoliceSystem.ts
+++ src/systems/PoliceSystem.ts
@@ -7,25 +7,22 @@ export default class PoliceSystem {
   constructor(scene: Phaser.Scene) {
     this.scene = scene;
     this.units = this.scene.physics.add.group({ classType: Phaser.Physics.Arcade.Sprite, maxSize: 20 });
-    // Animations for police
-    this.scene.anims.create({
-      key: 'police-walk',
-      frames: this.scene.anims.generateFrameNumbers('police', { start: 0, end: 3 }),
-      frameRate: 10,
-      repeat: -1
-    });
   }
 
   updateHeat(heat: number, player: Phaser.Physics.Arcade.Sprite) {
     const level = Math.floor(heat / 20);
     for (let i = this.units.getLength(); i < level; i++) {
       const unit = this.units.getFirstDead(true, Phaser.Math.Between(0, 8000), Phaser.Math.Between(0, 6000), 'police') as Phaser.Physics.Arcade.Sprite;
-      unit.setData('hp', 100);
-      this.scene.physics.moveToObject(unit, player, 200);
-      this.scene.time.addEvent({ delay: 500, callback: () => {
+      if (unit) {
+        unit.setData('hp', 100);
         this.scene.physics.moveToObject(unit, player, 200);
-        unit.anims.play('police-walk', true); // Play chase animation
-      }, loop: true });
+        this.scene.time.addEvent({ delay: 500, callback: () => {
+          if (unit.active) {
+            this.scene.physics.moveToObject(unit, player, 200);
+            unit.anims.play('police-walk', true);
+          }
+        }, loop: true });
+      }
     }
 
     if (heat < 5) {
