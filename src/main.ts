ed17eaa089323b69ac29d2e75769ceab634233e7
audh25-lab <audh25@gmail.com>
2025-12-18 00:35:58 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,8 @@ const config: Phaser.Types.Core.GameConfig = {
     autoCenter: Phaser.Scale.CENTER_BOTH
   },
   backgroundColor: '#000000',
-  audio: { disableWebAudio: false }
+  audio: { disableWebAudio: false },
+  pipeline: { 'Tint': Phaser.Renderer.WebGL.Pipelines.TintPipeline } 
 };
 
 const game = new Phaser.Game(config);
@@ -28,3 +29,20 @@ window.addEventListener('resize', () => {
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/service-worker.js').catch(console.error);
 }
+
+// Load save on game start
+game.events.on('ready', () => {
+  const save = localStorage.getItem('rva-save');
+  if (save) {
+    const data = JSON.parse(save);
+    game.scene.start(data.island);
+    const scene = game.scene.getScene(data.island);
+    scene.events.on('create', () => {
+      scene['playerStats'].hp = data.hp;
+      scene['playerStats'].cash = data.cash;
+      scene['playerStats'].ammo = data.ammo;
+      scene['playerStats'].heat = data.heat;
+      scene['reputation'].load(data.reputation);
+    });
+  }
+});
