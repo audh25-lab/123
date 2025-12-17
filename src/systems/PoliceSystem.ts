02326fc0cf146c3c5eddd5b65d9962bbd99d7bda
audh25-lab <audh25@gmail.com>
2025-12-17 23:31:41 +0500

    Update PoliceSystem.ts

--- src/systems/PoliceSystem.ts
+++ src/systems/PoliceSystem.ts
@@ -2,13 +2,33 @@ import Phaser from 'phaser';
 
 export default class PoliceSystem {
   private scene: Phaser.Scene;
+  private units: Phaser.Physics.Arcade.Group;
 
   constructor(scene: Phaser.Scene) {
     this.scene = scene;
+    this.units = this.scene.physics.add.group({ classType: Phaser.Physics.Arcade.Sprite, maxSize: 20 });
   }
 
   updateHeat(heat: number, player: Phaser.Physics.Arcade.Sprite) {
-    if (heat > 20) {
+    const level = Math.floor(heat / 20);
+    for (let i = this.units.getLength(); i < level; i++) {
+      const unit = this.units.getFirstDead(true, Phaser.Math.Between(0, 8000), Phaser.Math.Between(0, 6000), 'police') as Phaser.Physics.Arcade.Sprite;
+      unit.setData('hp', 100);
+      this.scene.physics.moveToObject(unit, player, 200);
+      this.scene.time.addEvent({ delay: 500, callback: () => this.scene.physics.moveToObject(unit, player, 200), loop: true });
     }
+
+    if (heat < 5) {
+      this.scene.time.delayedCall(10000, () => {
+        this.units.getChildren().forEach(u => u.destroy());
+      });
+    }
+
+    this.scene.physics.add.collider(this.units, player, () => {
+      this.scene['playerStats'].hp -= 10;
+      if (this.scene['playerStats'].hp <= 0) {
+        this.scene.scene.pause();
+      }
+    });
   }
 }
